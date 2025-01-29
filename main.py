de4f01523fd0751e3838350f1d6c7a3d0e900c4e
Bere_by35 <bdwbwad@gmail.com>
2025-01-29 09:45:04 +0200

    v2

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
+import random
+from tkinter import *
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+SIZE = 400
+GRID_LEN = 4
+GRID_PADDING = 10
+BACKGROUNG_COLOR = "#92877d"
+BACKGROUNG_COLOR_CELL_EMPTY = "#9e948a"
+BACKGROUNG_COLOR_DICT = {2 : "eee4da", 4:"#ede0c8", 8:"#f2b179", 16:"#f59563", 32:"#f67c5f",
+                         64:"#f65e3b", 128:"#edcf72", 256:"#edcc61", 512:"#edc850", 1024:"#edc53f", 2048:"#edc22e"}
+CELL_COLOR_DICT = {2:"#776e65", 4:"#776e65", 8:"#f9f6f2", 16:"#f9f6f2", 32:"#f9f6f2",
+                         64:"#f9f6f2", 128:"#f9f6f2", 256:"#f9f6f2", 512:"#f9f6f2", 1024:"#f9f6f2", 2048:"#f9f6f2"}
+FONT = ("Aria", 40, "bold")
+KEY_UP = "w"
+KEY_DOWN = "s"
+KEY_LEFT = "a"
+KEY_RIGHT = "d"
 
+matrix = []
+grid_cell = []
+mainframe = Frame()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def init_grid():
+    background = Frame(bg=BACKGROUNG_COLOR, width=SIZE, height=SIZE)
+    background.grid()
+    mainloop()
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+init_grid()
